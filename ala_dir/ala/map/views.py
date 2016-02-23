c8e43df02caff93a70eb44e9b575af4ab9698d64
andriesd <andriesd@umich.edu>
2016-02-23 10:35:02 -0500

    hard coded static files

--- ala_dir/ala/map/views.py
+++ ala_dir/ala/map/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
 
+from .models import (DataSet, QuantitativeVariable, QuantitativeVariable, Category, Breakpoint)
+
+#def index(request):
+    #template =loader.get_template('map/map.html')
+    #context = RequestContext(request)
+    #return HttpResponse(template.render(context))
 
 def index(request):
-    template =loader.get_template('map/map.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+	data = DataSet.objects.all()
+	return render(request, 'map/map.html')
