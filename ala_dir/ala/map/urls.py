6a6b796b9f5535076ad8651b9a082dda49c4c46a
andriesd <andriesd@umich.edu>
2016-02-23 12:24:08 -0500

    csv template progress

--- ala_dir/ala/map/urls.py
+++ ala_dir/ala/map/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^geojson/$', views.geojson, name='geojson'),
+	url(r'^csv/$', views.csv, name='csv'),
     url(r'^$', views.index, name='index'),
 ]
--- ala_dir/ala/map/views.py
+++ ala_dir/ala/map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
+import csv
 
 from .models import (DataSet, QuantitativeVariable, QuantitativeVariable, Category, Breakpoint)
 
@@ -20,3 +21,17 @@ def index(request):
 
 def geojson(request):
 	return render(request,'map/units.json')
+
+def csv(request):
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename = "data.csv"'
+
+	csv_data = loader.get_template('media/{{DataSet.csv}}')
+
+	t = loader.get_template('map/data_template.txt')
+	c = RequestContext(request, {
+		'data': csv_data
+	})
+	response.write(t.render(c))
+	return response
+	
